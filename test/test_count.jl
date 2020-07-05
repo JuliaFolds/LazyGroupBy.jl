efb318f6c22a9688538409c51ee41dd8dc583ca2
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-04 17:18:46 -0700

    Fix count (#19)

--- test/test_count.jl
+++ test/test_count.jl
@@ -8,6 +8,10 @@ using LazyGroupBy
           Dict(false => 3, true => 7)
     @test count.(grouped(identity, [true, false, true, true, true])) ==
           Dict(false => 0, true => 4)
+    @test_throws(
+        Exception,
+        count.(_ -> 0.5, grouped(isodd, [0, 7, 3, 1, 5, 9, 4, 3, 0, 5])),
+    )
 end
 
 end  # module
