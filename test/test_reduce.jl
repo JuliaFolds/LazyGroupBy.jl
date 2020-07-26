21fef11ef016534ee9a1a6f9c283e951caf67a25
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-26 09:29:04 -0700

    Support foldxl etc.

--- test/test_reduce.jl
+++ test/test_reduce.jl
@@ -2,6 +2,8 @@ module TestReduce
 
 using Test
 using LazyGroupBy
+using LazyGroupBy: foldxl, foldxt
+using Transducers: Map
 
 extrema_rf((min1, max1), (min2, max2)) = (min(min1, min2), max(max1, max2))
 
@@ -11,6 +13,16 @@ extrema_rf((min1, max1), (min2, max2)) = (min(min1, min2), max(max1, max2))
         extrema_rf,
         grouped(isodd, [0, 7, 3, 1, 5, 9, 4, 3, 0, 5]),
     ) == Dict(false => (0, 4), true => (1, 9))
+
+    @testset for fold in [foldl, foldxl, foldxt, reduce]
+        foldl = nothing
+
+        @test fold.(
+            extrema_rf,
+            Map(x -> (x, x)),
+            grouped(isodd, [0, 7, 3, 1, 5, 9, 4, 3, 0, 5]),
+        ) == Dict(false => (0, 4), true => (1, 9))
+    end
 end
 
 end  # module
